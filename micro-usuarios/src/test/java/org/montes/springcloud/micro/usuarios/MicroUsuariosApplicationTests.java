8d0fbc2d987acc87a8e31d18ab080055494f9e6f
Diego Montes <dgmontesg.12@gmail.com>
2023-04-12 22:52:14 -0700

    Primer commit agregando micro-usuarios

---
+++ micro-usuarios/src/test/java/org/montes/springcloud/micro/usuarios/MicroUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.montes.springcloud.micro.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
