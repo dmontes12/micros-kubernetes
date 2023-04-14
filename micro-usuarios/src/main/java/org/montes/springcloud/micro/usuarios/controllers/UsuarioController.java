fb1c0a98e1504d13d32a92c469fb3d1e78e1146b
Diego Montes <dgmontesg.12@gmail.com>
2023-04-13 22:27:06 -0700

    Creando la clase UsuarioController y agregando CRUD

---
+++ micro-usuarios/src/main/java/org/montes/springcloud/micro/usuarios/controllers/UsuarioController.java
@@ -0,0 +1,66 @@
+package org.montes.springcloud.micro.usuarios.controllers;
+
+
+import org.montes.springcloud.micro.usuarios.models.entity.Usuario;
+import org.montes.springcloud.micro.usuarios.services.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private IUsuarioService service;
+
+
+    @GetMapping
+    public List<Usuario> listar(){
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalle(@PathVariable Long id){
+        Optional<Usuario> usuarioOptional = service.porId(id);
+        if(usuarioOptional.isPresent()){
+            return ResponseEntity.ok(usuarioOptional.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody Usuario usuario){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> modificar(@RequestBody Usuario usuario, @PathVariable Long id){
+        Optional<Usuario> usuarioOptional = service.porId(id);
+        if(usuarioOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.guardar(usuario));
+        }else{
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        Optional<Usuario> usuarioOptional = service.porId(id);
+        if(usuarioOptional.isPresent()){
+            service.elimiar(id);
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+
+
+
+}
