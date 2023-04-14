2092b233c0e09f89fb853373a07f676a8cee71b4
Diego Montes <dgmontesg.12@gmail.com>
2023-04-13 21:43:52 -0700

    Creando el Repository Usuario y Service e ServiceImpl de Usuario

---
+++ micro-usuarios/src/main/java/org/montes/springcloud/micro/usuarios/services/IUsuarioService.java
@@ -0,0 +1,16 @@
+package org.montes.springcloud.micro.usuarios.services;
+
+import org.montes.springcloud.micro.usuarios.models.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUsuarioService {
+    List<Usuario> listar();
+    Optional<Usuario> porId(Long id);
+    Usuario guardar(Usuario usuario);
+    void elimiar(Long id);
+
+
+
+}
---
+++ micro-usuarios/src/main/java/org/montes/springcloud/micro/usuarios/services/UsuarioServiceImpl.java
@@ -0,0 +1,40 @@
+package org.montes.springcloud.micro.usuarios.services;
+
+import org.montes.springcloud.micro.usuarios.models.entity.Usuario;
+import org.montes.springcloud.micro.usuarios.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements IUsuarioService{
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Override
+    @Transactional(readOnly = true)
+    public List<Usuario> listar() {
+        return (List<Usuario>) usuarioRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Usuario> porId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Usuario guardar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    @Transactional
+    public void elimiar(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
